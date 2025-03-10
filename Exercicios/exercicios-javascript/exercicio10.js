e1431fa320fbe77b8c6055895d83e5d5af938ee1
Renata Carvalho <renataa-cs@gmail.com>
2025-03-09 21:20:34 -0300

    add exercicio 10

--- Exercicios/exercicios-javascript/exercicio10.js
+++ Exercicios/exercicios-javascript/exercicio10.js
@@ -7,3 +7,12 @@
 
 let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] 
 
+let numerosPares = numeros.filter(numero => numero % 2 === 0);
+
+let numerosMultiplicados = numerosPares.map(numero => numero * 2);
+
+let soma = numerosMultiplicados.reduce((acc, numero) => acc + numero, 0);
+
+console.log("Números pares:", numerosPares);
+console.log("Números multiplicados por 2:", numerosMultiplicados);
+console.log("Soma de todos os números obtidos:", soma);
