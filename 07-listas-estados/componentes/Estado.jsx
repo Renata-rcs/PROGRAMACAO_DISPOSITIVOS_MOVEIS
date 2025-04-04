eea5d215e3e20e933e59813407f268d709594ddf
Renata Carvalho <renataa-cs@gmail.com>
2025-04-04 20:33:15 -0300

    Listas e Estados

---
+++ 07-listas-estados/componentes/Estado.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { FlatList, StyleSheet, View } from "react-native";
+import { Card, Text } from "react-native-paper";
+import Municipio from "./Municipio";
+
+export default function Estado(props) {
+
+  const { nome, sigla, descricao, imagem, municipios } = props;
+
+  return (
+    <Card style={{margin: 10}}>
+      <Card.Title title={nome} subtitle={sigla} />
+      <Card.Content>
+        <Text>{descricao}</Text>
+      </Card.Content>
+      <Card.Cover source={{ uri: imagem }} />
+      <Card.Actions>
+        <Text>Teste</Text>
+      </Card.Actions>
+        <FlatList 
+            horizontal
+            data={municipios}
+            renderItem={({ item}) => (
+                <Municipio
+                    nome={item.nome}
+                    imagem={item.imagem}
+                />
+            )}
+        />
+    </Card>
+  );
+}
+
+const styles = StyleSheet.create({});
